0dab7c85c2d9a5c4c068ce055a775e27d21f34b6
buffontiago <89114431+buffontiago@users.noreply.github.com>
2021-09-09 21:49:01 -0300

    teste do sever

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ app.set("view engine", "ejs");       // função para renderizar as views
 app.use(express.static("public"));   // função para uzar o CSS e JS
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req, res){        //função para renderizar a pagina de login
   res.render("index");
 });
 
-app.post("/", function(req, res){
-  var login = req.body.nome;
-  var password = req.body.senha;
+//app.post("/", function(req, res){
+  //var login = req.body.nome;
+  //var password = req.body.senha;
 
 
 })
@@ -63,8 +63,6 @@ app.get("/inventario", function(req, res){
 
 })
 
-
-
- app.listen(process.env.PORT ||3000, function(){     //função para definir as portas do servidor
-  console.log("Servidor rodando");
+app.listen(process.env.PORT ||3000, function(){     //função para definir as portas do servidor
+console.log("Servidor rodando");
 });
