6d28778497428505524ad6e5e61eed8a77fc2b8f
buffontiago <89114431+buffontiago@users.noreply.github.com>
2021-09-20 09:25:38 -0300

    teste db-servidor

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");      // função para renderizar as views
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));   // função para uzar o CSS e JS
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+//mongoose.connect("mongodb://localhost:27017/userDB");
 
 
 app.get("/", function(req, res){        //função para renderizar a pagina de login
@@ -69,52 +69,52 @@ app.get("/inventario", function(req, res){
 
 
 
-const userSchema = {
-  loginNewUser: String,
-  email: String,
-  nomeCompleto: String,
-  password: String,
-  dataNascimento: Date
-};
-
-const User = new mongoose.model("User", userSchema);
-
-app.post("/novoUsuario", function (req, res){
-  const newUser = new User({
-    loginNewUser: req.body.loginNewUser,
-    email: req.body.email,
-    nomeCompleto: req.body.nomeCompleto,
-    password: req.body.password,
-    dataNascimento: req.body.dataNascimento
-
-      });
-
-
-  newUser.save(function (err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("paginaPrincipal");
-    }
-});
-});
-
-app.post("/", function(req, res){
-  const login = req.body.login;
-  const password = req.body.password;
-
-  User.findOne({loginNewUser: login}, function(err, foundUser){
-   if (err){
-    console.log(err);
-    }else{
-      if (foundUser){
-        if(foundUser.password === password){
-         res.render("paginaPrincipal");
-        }
-      }
-    }
-  });
-});
+// const userSchema = {
+//   loginNewUser: String,
+//   email: String,
+//   nomeCompleto: String,
+//   password: String,
+//   dataNascimento: Date
+// };
+//
+// const User = new mongoose.model("User", userSchema);
+//
+// app.post("/novoUsuario", function (req, res){
+//   const newUser = new User({
+//     loginNewUser: req.body.loginNewUser,
+//     email: req.body.email,
+//     nomeCompleto: req.body.nomeCompleto,
+//     password: req.body.password,
+//     dataNascimento: req.body.dataNascimento
+//
+//       });
+//
+//
+//   newUser.save(function (err){
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.render("paginaPrincipal");
+//     }
+// });
+// });
+//
+// app.post("/", function(req, res){
+//   const login = req.body.login;
+//   const password = req.body.password;
+//
+//   User.findOne({loginNewUser: login}, function(err, foundUser){
+//    if (err){
+//     console.log(err);
+//     }else{
+//       if (foundUser){
+//         if(foundUser.password === password){
+//          res.render("paginaPrincipal");
+//         }
+//       }
+//     }
+//   });
+// });
 
 
 app.listen(process.env.PORT ||3000, function(){     //função para definir as portas do servidor
