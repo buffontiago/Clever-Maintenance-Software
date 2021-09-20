2855061d59fb04bce6f6fbce8c609683e37792f4
buffontiago <89114431+buffontiago@users.noreply.github.com>
2021-09-20 08:31:52 -0300

    Novo usuário DB
    salva e acha o login e password

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const express = require ("express");   //definir a função para requerir os modulos do node-express
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
-const app = express();              //constante para criar a função express
+const app = express();
 
 app.set("view engine", "ejs");      // função para renderizar as views
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));   // função para uzar o CSS e JS
 
-
-//app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/userDB");
 
 
 app.get("/", function(req, res){        //função para renderizar a pagina de login
   res.render("index");
 });
 
-app.post("/", function(req, res){
-  var login = req.body.nome;
-  var password = req.body.senha;
-
-
-});
 
 app.get("/novoUsuario", function(req, res){
   res.render("novoUsuario");
@@ -70,6 +64,58 @@ app.get("/inventario", function(req, res){
   res.render("inventario");
 
 });
+
+//função para o banco de dados do registro de usuário
+
+
+
+const userSchema = {
+  loginNewUser: String,
+  email: String,
+  nomeCompleto: String,
+  password: String,
+  dataNascimento: Date
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.post("/novoUsuario", function (req, res){
+  const newUser = new User({
+    loginNewUser: req.body.loginNewUser,
+    email: req.body.email,
+    nomeCompleto: req.body.nomeCompleto,
+    password: req.body.password,
+    dataNascimento: req.body.dataNascimento
+
+      });
+
+
+  newUser.save(function (err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("paginaPrincipal");
+    }
+});
+});
+
+app.post("/", function(req, res){
+  const login = req.body.login;
+  const password = req.body.password;
+
+  User.findOne({loginNewUser: login}, function(err, foundUser){
+   if (err){
+    console.log(err);
+    }else{
+      if (foundUser){
+        if(foundUser.password === password){
+         res.render("paginaPrincipal");
+        }
+      }
+    }
+  });
+});
+
 
 app.listen(process.env.PORT ||3000, function(){     //função para definir as portas do servidor
 console.log("Servidor rodando");
